60d33c4f1a0215272d42e5bfa6085280a85aa391
samyam81 <samyam081@gmail.com>
2024-07-11 14:52:04 +0545

    Email Validation

--- FormValidation.php
+++ FormValidation.php
@@ -1,14 +1,26 @@
 <?php
+    // Name Validation
     if (empty($_POST["name"])) {
-    #code....
-    $nameErr = "Name is Required";
-} else {
+        #code....
+        $nameErr = "Name is Required";
+    } else {
     $name = input_data($_POST["name"]);
     #code....
-    if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
-        $nameErr = "Only Alphabets and White Space are Allowed";
+        if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+            $nameErr = "Only Alphabets and White Space are Allowed";
+        }
     }
-}
+
+    //Email Validation
+    if(empty($_POST["email"])){
+        $emailErr="Email is Required";
+    } else{
+        $emailErr=input_data($_POST["email"]);
+        if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+            # code...
+            $emailErr="Invalid Email Format";
+        }
+    }
 
     function input_data($data){
         $data=trim($data);
