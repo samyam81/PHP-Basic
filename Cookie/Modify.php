0121e42e0a146770b144d1e0497bfb60a32def25
samyam81 <samyam081@gmail.com>
2024-07-11 15:34:02 +0545

    Completing Cookie

---
+++ Cookie/Modify.php
@@ -0,0 +1,19 @@
+<?php
+    $Cookie_name="User";
+    $Cookie_value="Aarvii";
+    setcookie($Cookie_name,$Cookie_value,time()+(86400*30),"/");
+?>
+<html>
+    <body>
+        <?php
+            if (!isset($_COOKIE[$Cookie_name])) {
+                # code... 
+                echo "Cookie Name: ".$Cookie_name." is not set!<br>";
+            } else {
+                # code...
+                echo "Cookie Name: ".$Cookie_name." is set!<br>";
+                echo "Cookie is: ".$Cookie_value."<br>";
+            }
+        ?>
+    </body>
+</html>
